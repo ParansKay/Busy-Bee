2634e6f83b085c220932311d008d4cb6565cdbc3
Paran Kashani <parankashani@gmail.com>
2022-11-28 10:59:09 -0600

    initial modal styling

--- src/components/TaskList/TaskList.jsx
+++ src/components/TaskList/TaskList.jsx
@@ -1,7 +1,9 @@
 import {useState, useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { Link, useHistory, useNavigate } from 'react-router-dom'; //must define link within each component, otherwise we get an undefined error
+import { Box, Modal } from '@mui/material';
 import './TaskList.css'
+import NewTask from '../NewTask/NewTask';
 
 function TaskList() {
     const dispatch = useDispatch();
@@ -16,8 +18,11 @@ function TaskList() {
         history.push("/");
         }, 300);
 
-    //handle open 
-    //handle close
+    //HANLDE POP-UP MODAL
+    const [open, setOpen] = useState(false);
+    const handleClickOpen = () => {
+        setOpen(!open);
+    };
 
     const deleteTask = (id) => () =>{ //the extra () and => is so that convert handleDelete to a curried function to close over the post id in callback scope and return an onClick handler function.
         console.log();
@@ -29,7 +34,6 @@ function TaskList() {
             dispatch({ type: 'FETCH_TASKS'});
             }, 100);
       }
-
 
     return (
         <main className='listDisplayMain'>
@@ -46,8 +50,29 @@ function TaskList() {
                 })}
             </section>
             <div className='addNewTodoBtnDiv'>
-                <button className='mainBtn'>Add a new to-do!</button>
+                <button className='mainBtn' onClick={handleClickOpen}>Add a new to-do!</button>
             </div>
+            <div>
+                <Modal
+                open={open}
+                onClose={handleClickOpen}
+                className="createNewTaskModal"
+                style={{
+                display: 'flex',
+                flexDirection:'column',
+                alignContent: 'center',
+                position: 'flexible',
+                top: '15%',
+                left: '0',
+                marginLeft: '25%',
+                marginRight: '25%',
+                outline: 'none'
+                }}>
+                <div>
+                    <NewTask/>  
+                </div> 
+                </Modal>
+      </div>
         </main>
 
     );
